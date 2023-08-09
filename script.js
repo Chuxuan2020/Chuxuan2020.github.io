a3232c2153384e78e4c549adeb64abc72de2fb3c
chuxuan <chuxuanli2020@u.northwestern.edu>
2023-08-09 14:29:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 // Get the navigation element
 var navigation = document.querySelector('.navigation');
 
-// Get the offset position of the navigation
-var navOffset = navigation.offsetTop;
+// Check if the navigation element exists before proceeding
+if (navigation) {
+  // Get the offset position of the navigation
+  var navOffset = navigation.offsetTop;
 
-// Function to update the navigation's position
-function updateNavigationPosition() {
-  if (window.pageYOffset >= navOffset) {
-    navigation.style.top = '0';
-  } else {
-    navigation.style.top = '-50px'; /* Adjust this value based on your navigation's height */
+  // Function to update the navigation's position
+  function updateNavigationPosition() {
+    if (window.pageYOffset >= navOffset) {
+      navigation.style.top = '0';
+    } else {
+      navigation.style.top = '-50px'; /* Adjust this value based on your navigation's height */
+    }
   }
-}
 
-// Add an event listener to the "scroll" event
-window.addEventListener('scroll', updateNavigationPosition);
+  // Add an event listener to the "scroll" event
+  window.addEventListener('scroll', updateNavigationPosition);
+} else {
+  console.error("Navigation element not found.");
+}
 
